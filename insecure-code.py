f7832b45d40dcbb2153eeb65afd560f0ddd3ab71
Adreeta Maity <adrita1116@gmail.com>
2023-11-02 17:52:36 +0530

    adding insecure code2

--- insecure-code.py
+++ insecure-code.py
@@ -1,8 +1,20 @@
+import sqlite3
+
 def insecure_code():
+    # Insecure: Hardcoded password
     password = "my_password"
     print(f"Password is: {password}")
 
+    # Insecure: Hardcoded secret key
     secret_key = "super_secret_key"
     print(f"Secret key is: {secret_key}")
+
+    # Insecure: Using unsanitized user input in an SQL query
+    user_input = input("Enter a name: ")
+    conn = sqlite3.connect("mydb.db")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM users WHERE name = '{user_input}'")
+    results = cursor.fetchall()
+    print("Results:", results)
 
 insecure_code()
