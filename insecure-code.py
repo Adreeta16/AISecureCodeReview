9a3ce0da4f064b7719548f2e38f35094d9b4917a
Adreeta Maity <adrita1116@gmail.com>
2023-11-02 17:45:28 +0530

    adding insecure code

---
+++ insecure-code.py
@@ -0,0 +1,8 @@
+def insecure_code():
+    password = "my_password"
+    print(f"Password is: {password}")
+
+    secret_key = "super_secret_key"
+    print(f"Secret key is: {secret_key}")
+
+insecure_code()
